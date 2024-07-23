924f86112111bc98150e2d65e821b3849984d8b6
Olek Salko <ukrsalko@gmail.com>
2024-07-23 15:10:02 +0300

    Solve javascript.info 5.1-5.2 tasks

---
+++ types.js
@@ -0,0 +1,33 @@
+"use strict"
+
+// Types 2
+
+// let first = +prompt("Перше число?", 0);
+// let second = +prompt("Друге число?", 0);
+
+// alert(first+second);
+
+// function readNumber() {
+//     let number;
+
+//     while (true) {
+//         number = prompt("Введіть число", 0);
+//         if (number == "" || number === null) {
+//             return null;
+//         } else if (isFinite(number)) return +number;
+//     }
+// }
+
+// alert(readNumber());
+
+// function random(min, max){
+//     return Math.random() * (max - min) + min; 
+// }
+
+// alert( random(7, 10) );
+
+// function randomInteger(min, max){
+//     return Math.trunc(min + Math.random() * (max + 1 - min));
+// }
+
+// alert( randomInteger(1, 5) );
