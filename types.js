3851b82af79abd03a1ea4738ffeaf24f27ef9298
Olek Salko <ukrsalko@gmail.com>
2024-07-23 18:29:07 +0300

    Solve javascript.info 5.4 tasks

--- types.js
+++ types.js
@@ -1,5 +1,63 @@
 "use strict"
 
+// Types 4
+
+// function getMaxSubSum(arr) {
+//     let maxSum = 0;
+//     let partialSum = 0;
+  
+//     for (let item of arr) { // for each item of arr
+//       partialSum += item; // add it to partialSum
+//       maxSum = Math.max(maxSum, partialSum); // remember the maximum
+//       if (partialSum < 0) partialSum = 0; // zero if negative
+//     }
+  
+//     return maxSum;
+//   }
+  
+//   alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+//   alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+//   alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+//   alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+//   alert( getMaxSubSum([1, 2, 3]) ); // 6
+//   alert( getMaxSubSum([-1, -2, -3]) ); // 0
+
+// function sumInput(){
+//     let numbers = [];
+
+//     while (true){
+//         let num = prompt("Vvedit chislo?");
+
+//         if (num == null || num == '' || !isFinite(num)) break;
+        
+//         numbers.push(+num);
+//     }
+
+//     let sum = 0;
+//     for(let num of numbers){
+//         sum += num;
+//     }
+
+//     return sum;
+// }
+
+// alert(sumInput());
+
+// let arr = ["a", "b"];
+
+// arr.push(function() {
+//   alert( this );
+// })
+
+// arr[2](); // a,b,function(){...}
+
+// let styles = ["Jazz", "Blues"];
+// styles.push("Rock-n-Roll");
+// styles[Math.round(styles.length - 1) / 2] = "Classics";
+// alert(styles.shift());
+// styles.unshift("Rap", "Reggae")
+// alert(styles);
+
 // Types 3
 
 // function extractCurrencyValue(str){
