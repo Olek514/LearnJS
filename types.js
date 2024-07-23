d13e6856b7eed7d529cf22a05cea2f2c159093eb
Olek Salko <ukrsalko@gmail.com>
2024-07-23 17:27:13 +0300

    Solve javascript.info 5.3 tasks

--- types.js
+++ types.js
@@ -1,5 +1,37 @@
 "use strict"
 
+// Types 3
+
+// function extractCurrencyValue(str){
+//     return +str.slice(1);
+// }
+
+// alert( extractCurrencyValue('$120') === 120 ); // true
+
+// function truncate(str, maxlength){
+//     return (str.length > maxlength) ?
+//     str.slice(0, maxlength - 1) + '…' : str;
+// }
+
+// alert( truncate("Що я хотів би розповісти на цю тему:", 20));// == "Що я хотів би розпо…"
+
+// alert( truncate("Всім привіт!", 20));// == "Всім привіт!"
+
+// function checkSpam(str){
+//     str = str.toUpperCase();
+//     return str.includes('XXX') || str.includes('VIAGRA');
+// }
+
+// alert( checkSpam('buy ViAgRA now') == true);
+// alert( checkSpam('free xxxxx') == true);
+// alert( checkSpam("innocent rabbit") == false);
+
+// function ucFirst(str){
+//     return str[0].toUpperCase() + str.slice(1);
+// }
+
+// alert( ucFirst("василь") );
+
 // Types 2
 
 // let first = +prompt("Перше число?", 0);
